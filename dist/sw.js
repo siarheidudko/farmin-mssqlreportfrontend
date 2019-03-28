f1176db35238f19a87ced40a32c06c85e84dd2ca
Siarhei Dudko <admin@sergdudko.tk>
2019-03-28 18:18:37 +0300

    upd

--- dist/sw.js
+++ dist/sw.js
@@ -1,5 +1,5 @@
 'use strict';
 importScripts('sw-toolbox.js'); 
-toolbox.precache(['index.php','mssql-report.css','daypicker.css','mssql-report.js','sw.js','sw-toolbox.js', '../images/fon2.jpg']); 
+toolbox.precache(['index.php','mssql-report.css','daypicker.css','mssql-report.js','sw.js','sw-toolbox.js', '../images/fon1.jpg']); 
 toolbox.router.get('pwa/*', toolbox.cacheFirst); 
 toolbox.router.get('*', toolbox.networkFirst, { networkTimeoutSeconds: 5});
